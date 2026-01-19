ef344abbafafc127d9c9a6b694c58a2840e2a3cf
Manasa L <ml7065711@gmail.com>
2026-01-19 18:01:19 +0530

    Largest_of_two_numbers.c

---
+++ Largest_of_two_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &a, &b);
+
+    if (a > b) {
+        printf("Largest number = %d\n", a);
+    } else if (b > a) {
+        printf("Largest number = %d\n", b);
+    } else {
+        printf("Both numbers are equal\n");
+    }
+
+    return 0;
+}
